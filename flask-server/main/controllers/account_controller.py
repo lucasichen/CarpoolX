88c80f45761a639d6e17d05a7c95fbc672676aac
Lucas Chen <lucas.chen2016@gmail.com>
2023-03-31 01:12:49 -0400

    fix login backend

--- flask-server/main/controllers/account_controller.py
+++ flask-server/main/controllers/account_controller.py
@@ -1,45 +1,35 @@
-from main.model.firebase_controller import FirebaseInit, FirebaseAuth, FirebaseDatabase
+from main.model.firebase_controller import FirebaseAuth, FirebaseDatabase
 
 class AccountController:
     def __init__(self):
-        self.firebase_init = FirebaseInit()
         self.firebase_auth = FirebaseAuth()
-        # self.firebase_db = FirebaseDatabase()
-
-        
+        self.firebase_db = FirebaseDatabase()
 
     def create_account(self, email, password):
-        # try:
-            # Create user in Firebase Auth
-            user = self.firebase_auth.create_user(email, password)
-
+        try:
             # Check if user already exists in users collection
-            # existing_user = self.firebase_db.get_document_by_field('users', 'email', email)
+            existing_user = self.firebase_db.user_exists(email)
 
-            # if existing_user:
-            #     return {'success': True, 'message': 'User created successfully!'}
-            # else:
-            #     # Create new user document in users collection
-            #     new_user = {
-            #         'uid': user.uid,
-            #         'email': email,
-            #     }
-            #     self.firebase_db.add_document('users', new_user)
+            if existing_user:
+                return {'success': True, 'message': 'User already exists!'}
+            else:
+                 # Create user in Firebase Auth
+                user = self.firebase_auth.create_user(email, password)
+                # Create new user document in users collection
+                self.firebase_db.create_user(user, email, password)
             return {'success': True, 'message': 'User created successfully!'}
-        # except Exception as e:
-        #     return {'success': False, 'error': str(e)}
+        except Exception as e:
+            return {'success': False, 'error': str(e)}
 
-    # def login(self, email, password):
-    #     try:
-    #         # Sign in user with Firebase Auth
-    #         user = self.firebase_auth.sign_in_user(email, password)
+    def login(self, email, password):
+        try:
+            print(email, password)
+            # Check if user already exists in users collection
+            response = self.firebase_auth.sign_in(email, password)
+            if response['success']:
+                return {'success': True, 'message': 'User logged in successfully!'}
+            else:
+                return {'success': False, 'error': response['error']}
+        except Exception as e:
+            return {'success': False, 'error': str(e)}
 
-    #         # Get user document from users collection
-    #         user_doc = self.firebase_db.get_document_by_field('users', 'uid', user.uid)
-
-    #         if user_doc:
-    #             return {'success': True, 'user': user_doc}
-    #         else:
-    #             return {'success': False, 'error': 'User document not found.'}
-    #     except Exception as e:
-    #         return {'success': False, 'error': str(e)}
--- flask-server/main/model/firebase_controller.py
+++ flask-server/main/model/firebase_controller.py
@@ -1,50 +1,65 @@
-import firebase_admin
-from firebase_admin import auth, db, credentials
+# import firebase_admin
+# from firebase_admin import auth, credentials
+# from firebase_admin import firestore
+import pyrebase
 import os
+import json
 
 class FirebaseInit:
     def __init__(self):
-        credential_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"serviceAccountKey.json")
-        cred = credentials.Certificate(credential_file_path)
-        firebase_admin.initialize_app(cred)
+        pyrebase_config = os.path.join(os.path.dirname(os.path.abspath(__file__)),"serviceAccountKey.json")
+        # firebase_config = os.path.join(os.path.dirname(os.path.abspath(__file__)),"firebase_key.json")
+        with open(pyrebase_config) as f:
+            config = json.load(f)
+        # with open(firebase_config) as f:
+        #     firebase_config = json.load(f)
+        # cred = credentials.Certificate(firebase_config)
+        # self.firebase = firebase_admin.initialize_app(cred)
+        self.pyrebase = pyrebase.initialize_app(config)
 
-class FirebaseAuth:
+class FirebaseAuth(FirebaseInit):
     def __init__(self):
-        self.app = firebase_admin.get_app()
+        super().__init__()
+        self.auth = self.pyrebase.auth()
 
     def create_user(self, email, password):
-        user = auth.create_user(
+        user = self.auth.create_user_with_email_and_password(
             email=email,
-            password=password,
-            app=self.app
+            password=password
         )
-        return user.uid
+        return user['localId']
 
-class FirebaseDatabase:
+    def sign_in(self, email, password):
+        try:
+            user = self.auth.sign_in_with_email_and_password(
+                email=email,
+                password=password
+            )
+            return {'success': True, 'data': user}
+        except pyrebase.exceptions.HTTPError as e:
+            error_message = e.args[1].get('error', {}).get('message', '')
+            if error_message == 'INVALID_PASSWORD' or error_message == 'EMAIL_NOT_FOUND':
+                return {'success': False, 'error': 'Invalid email or password'}
+            else:
+                return {'success': False, 'error': error_message}
+
+class FirebaseDatabase(FirebaseInit):
     def __init__(self):
-        self.app = firebase_admin.get_app()
-        self.db = db.reference(app=self.app)
+        super().__init__()
+        self.db = self.pyrebase.database()
 
-    def create_user(self, uid, email, name):
-        user_ref = self.db.child('users').child(uid)
-        user_data = {
-            'email': email,
-            'name': name
-        }
-        user_ref.set(user_data, app=self.app)
-
-    def get_document_by_field(self, collection_name, field_name, field_value):
-        """
-        Retrieves a document from the specified collection based on a specific field value.
-        """
-        docs = self.db.child(collection_name).order_by_child(field_name).equal_to(field_value).get()
-        for doc in docs:
-            return docs[doc]
-        return None
+    def create_user(self, uid, email, password):
+        user_ref = self.db.child("user").child(uid).set({'email': email,'password': password})
+        print(user_ref)
     
-    def add_document(self, collection_name, data):
-        """
-        Adds a new document to the specified collection.
-        """
-        new_document_ref = self.db.child(collection_name).push(data)
-        return new_document_ref.key
+    def user_exists(self, email):
+        try:
+            users = self.db.child("user").get()
+            for user in users.each():
+                if user.val().get('email') == email:
+                    print('t')
+                    return True
+            return False
+        except Exception as e:
+            return False
+        
